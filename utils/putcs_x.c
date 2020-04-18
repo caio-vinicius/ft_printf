6da5430e6730aa2401b7ed6dd365075d6aeeda07
Caio vinicius Souza ferreira <csouza-f@c1r1p4.42sp.org.br>
2020-04-17 21:43:20 -0300

    fix last error

--- utils/putcs_x.c
+++ utils/putcs_x.c
@@ -6,7 +6,7 @@
 /*   By: csouza-f <csouza-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/12 10:42:38 by csouza-f          #+#    #+#             */
-/*   Updated: 2020/03/15 20:10:56 by csouza-f         ###   ########.fr       */
+/*   Updated: 2020/04/17 21:42:03 by csouza-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ int	putcs_x(char *str, int c, int x, int time, int nbr)
 	(nbr < 0 && x != 0) ? x-- : 0;
 	if (time == 0)
 	{
-		(nbr < 0) ? len += ft_putchar('-') : 0;
+		if (nbr < 0 && nbr >= -2147483647)
+			len += ft_putchar('-');
 		len += ft_putstr(str);
 		len += ft_putchar_x(c, x);
 	}
